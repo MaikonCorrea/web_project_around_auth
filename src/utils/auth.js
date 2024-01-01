f1b413c255471837a4369b826e92519784d69ecb
maikoncorrea <maikonacorrea@gmail.com>
2024-01-01 16:05:25 -0300

    refactored auth.js deleted then/catch

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,6 +1,6 @@
 const BASE_URL = 'https://register.nomoreparties.co';
 
-export const signup = (email, password) => {
+export const register = async ({ email, password }) => {
     return fetch(`${BASE_URL}/signup`, {
         method: 'POST', 
         headers: {
@@ -9,39 +9,20 @@ export const signup = (email, password) => {
         },
         body: JSON.stringify({ email, password })
     })
-        .then((response) => {
-            if (response.status === 201){
-                console.log(response.status)
-                return response.json();
-              }
-        })
-        .then((res) => {
-            console.log(res)
-            return res;
-        })
-        .catch((err) => console.log(err));
-    }
+} 
 
-export const signin = (identifier, password) => {
+export const authorize = async ({ email, password }) => {
     return fetch(`${BASE_URL}/signin`, {
         method: 'POST',
         headers: {
             'Accept': 'application/json',
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify({ identifier, password })
+        body: JSON.stringify({ email, password })
     })
-    .then((response => response.json()))
-    .then((data) => {
-        if (data.user) {
-            localStorage.setItem('jwt', data.jwt);
-            return data;
-        }
-    })
-    .catch((err => console.log(err)))
 }
 
-export const checkToken = (token) => {
+export const checkToken = async (token) => {
     return fetch(`${BASE_URL}/users/me`, {
         method: 'GET',
         headers: {
@@ -50,6 +31,4 @@ export const checkToken = (token) => {
             'Authorization': `Bearer ${token}`,
         }
     })
-    .then((res => res.json()))
-    .then((data => data))
 }
