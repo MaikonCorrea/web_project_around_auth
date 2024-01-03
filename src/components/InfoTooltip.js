07a1ff2895475c3609625ab837598a23be0efe24
maikoncorrea <maikonacorrea@gmail.com>
2024-01-03 17:03:15 -0300

    created soft and conditional closing and opening functions for text and image content in infoTooltip.js

--- src/components/InfoTooltip.js
+++ src/components/InfoTooltip.js
@@ -1,20 +1,83 @@
-
-
+import React, { useState, useEffect } from "react";
 import imageError from "../images/image_error.png";
 import imageOk from "../images/image_ok.png";
 
+function InfoTooltip({ isSuccess, onClose, isOpen }) {
+  const [shouldRenderPopup, setShouldRenderPopup] = useState(false);
+  const [isClosing, setIsClosing] = useState(false);
 
-function InfoTooltip() {
+  useEffect(() => {
+    const handleEscape = (event) => {
+      if (event.key === "Escape" && isOpen) {
+        setIsClosing(true);
+
+        setTimeout(() => {
+          setIsClosing(true);
+          onClose();
+        }, 200);
+      }
+    };
+
+    document.addEventListener("keydown", handleEscape);
+
+    return () => {
+      document.removeEventListener("keydown", handleEscape);
+    };
+  }, [isOpen, onClose]);
+
+  useEffect(() => {
+    if (isOpen) {
+      setIsClosing(false);
+
+      const timeoutId = setTimeout(() => {
+        setShouldRenderPopup(true);
+      }, 150);
+
+      return () => {
+        clearTimeout(timeoutId);
+      };
+    } else {
+      setIsClosing(true);
+      setShouldRenderPopup(false);
+
+      const timeoutId = setTimeout(() => {
+        setIsClosing(false);
+      }, 150);
+
+      return () => {
+        clearTimeout(timeoutId);
+      };
+    }
+  }, [isOpen]);
+
+  const handleOverlayClick = (event) => {
+    if (event.target === event.currentTarget) {
+      event.preventDefault();
+      setIsClosing(true);
+      setTimeout(() => {
+        onClose();
+      }, 150);
+    }
+  };
+    
   return (
-    <div className="info">
+    <div className={`info ${shouldRenderPopup ? "info__popup_opened" : ""}${
+      isClosing ? "info__popup__closing" : ""
+    }`} onClick={handleOverlayClick} >
       <div className="info__popup">
-        {/* <img className="info__image" src={imageError} alt="imagem de erro" /> */}
-        <img className="info__image" src={imageOk} alt="imagem de sucesso" />
-        <p className="info__description">Vitória! Você precisa se registrar.</p>
-        {/* <p className="info__description">
-          Ops, algo saiu deu errado! Por favor, tente novamente.
-        </p> */}
-        <button className="info__button-close-popup button__close-popup"></button>
+        {isSuccess ? (
+          <img className="info__image" src={imageOk} alt="imagem de sucesso" />
+        ) : (
+          <img className="info__image" src={imageError} alt="imagem de erro" />
+        )}
+        {isSuccess ? (
+          <p className="info__description">Vitória! Você precisa se registrar.</p>
+        ) : (
+          <p className="info__description">
+            Ops, algo saiu deu errado! Por favor, tente novamente.
+          </p>
+        )}
+        <button className="info__button-close-popup button__close-popup" onClick={handleOverlayClick}></button>
       </div>
     </div>
   );
