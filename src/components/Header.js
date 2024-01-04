ca3325090de1a0aea7208df0a548d0e6d6069e8a
maikoncorrea <maikonacorrea@gmail.com>
2024-01-04 13:00:23 -0300

    created different menu for mobile changed header.js and header.css

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,54 @@
+import React, { useState, useEffect } from "react";
 import { Link, useHistory, useLocation } from "react-router-dom";
-
 import headerLogo from "../images/Vector_Around.png";
 
 export default function Header({ isLoggedIn, handleLogout, userEmail }) {
   const location = useLocation();
   const history = useHistory();
+  const [stateScreen, setStateScreen] = useState(false);
+  const [widthscreen, setWidthscreen] = useState(window.innerWidth);
+  const [menuOpen, setMenuOpen] = useState(false); // Novo estado para controlar a abertura/fechamento do menu
+
   function SignOut() {
-    handleLogout()
-    localStorage.removeItem('jwt')
-    history.push('/login')
+    handleLogout();
+    localStorage.removeItem("jwt");
+    history.push("/login");
+  }
+
+  useEffect(() => {
+    function handleResize() {
+      setWidthscreen(window.innerWidth);
+    }
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  useEffect(() => {
+    setStateScreen(widthscreen <= 588);
+  }, [widthscreen]);
+
+  function toggleMenu() {
+    setMenuOpen(!menuOpen);
   }
 
   return (
     <>
       <header className="header">
+        {(!stateScreen || menuOpen) &&
+          isLoggedIn &&
+          location.pathname === "/profile" && (
+            <div className="header__container--open">
+              <p className="header__menu-email">{userEmail}</p>
+              <button onClick={SignOut} className="header__menu-button">
+                Sair
+              </button>
+              {menuOpen && <div className="header__line"></div>}
+            </div>
+          )}
         <div className="header__container">
           <img className="header__logo" src={headerLogo} alt="logo do site" />
           <div className="header__menu">
@@ -29,16 +64,30 @@ export default function Header({ isLoggedIn, handleLogout, userEmail }) {
               </Link>
             )}
 
-            {isLoggedIn && location.pathname === "/profile" && (
-              <>
-                 <p className="header__menu-email">{userEmail}</p>
+            {!stateScreen && isLoggedIn && location.pathname === "/profile" && (
+              <div className="header__menu-container">
+                <p className="header__menu-email">{userEmail}</p>
                 <button onClick={SignOut} className="header__menu-button">
                   Sair
                 </button>
-              </>
+                {menuOpen && <div className="header__line"></div>}
+              </div>
+            )}
+            {stateScreen && isLoggedIn && location.pathname === "/profile" && (
+              <div className="header__menu-container__sandwich">
+                <button
+                  className={`header__menu-button ${
+                    menuOpen
+                      ? "header__menu-button-close"
+                      : "header__menu-button-open"
+                  }`}
+                  onClick={toggleMenu}
+                ></button>
+              </div>
             )}
           </div>
         </div>
+
         <div className="header__line"></div>
       </header>
     </>
