45c346ab342d8888290fbaf545e7da479a568c66
maikoncorrea <maikonacorrea@gmail.com>
2024-01-05 00:50:29 -0300

    Formatted code in auth.js

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,34 +1,34 @@
-const BASE_URL = 'https://register.nomoreparties.co';
+const BASE_URL = "https://register.nomoreparties.co";
 
 export const register = async ({ email, password }) => {
-    return fetch(`${BASE_URL}/signup`, {
-        method: 'POST', 
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ email, password })
-    })
-} 
+  return fetch(`${BASE_URL}/signup`, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ email, password }),
+  });
+};
 
 export const authorize = async ({ email, password }) => {
-    return fetch(`${BASE_URL}/signin`, {
-        method: 'POST',
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ email, password })
-    })
-}
+  return fetch(`${BASE_URL}/signin`, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ email, password }),
+  });
+};
 
 export const checkToken = async (token) => {
-    return fetch(`${BASE_URL}/users/me`, {
-        method: 'GET',
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${token}`,
-        }
-    })
-}
+  return fetch(`${BASE_URL}/users/me`, {
+    method: "GET",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  });
+};
